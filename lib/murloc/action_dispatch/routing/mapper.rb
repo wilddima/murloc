37655200b3a86c0da7f3ef1e9bdb6a077d52f385
Dmitrii Topornin <dtopornin@gmail.com>
2017-07-11 11:45:55 +0300

    change rubocop config, remove logs from dummy app

--- lib/murloc/action_dispatch/routing/mapper.rb
+++ lib/murloc/action_dispatch/routing/mapper.rb
@@ -1,28 +1,32 @@
-# Monkey patching for adding in locale urls
-class ActionDispatch::Routing::Mapper
-  def in_locales(locales, &block)
-    scope_name = '(:locale)'
-    scope_args = { locale: /#{locales.join("|")}/ }
+module ActionDispatch
+  module Routing
+    # Monkey patching for adding in locale urls
+    class Mapper
+      def in_locales(locales, &block)
+        scope_name = '(:locale)'
+        scope_args = { locale: /#{locales.join("|")}/ }
 
-    scope(scope_name, scope_args, &block)
+        scope(scope_name, scope_args, &block)
 
-    match '', to: redirect { |_, request| "#{I18n.locale}#{query_params(request)}" }, via: :all
+        match '', to: redirect { |_, request| "#{I18n.locale}#{query_params(request)}" }, via: :all
 
-    match '*path', to: redirect { |params, request|
-      "#{I18n.locale}#{path_params(params)}#{query_params(request)}"
-    }, constraints: { path: %r{(?!(#{I18n.available_locales.join("|")})\/).*} }, via: :all
-  end
+        match '*path', to: redirect { |params, request|
+          "#{I18n.locale}#{path_params(params)}#{query_params(request)}"
+        }, constraints: { path: %r{(?!(#{I18n.available_locales.join("|")})\/).*} }, via: :all
+      end
 
-  private
+      private
 
-  def query_params(request)
-    qp = request.query_parameters
-    "?#{qp.to_query}" if qp.present?
-  end
+      def query_params(request)
+        qp = request.query_parameters
+        "?#{qp.to_query}" if qp.present?
+      end
 
-  def path_params(params)
-    "/#{params[:path]
-          .gsub(/^#{I18n.available_locales.join("|")}/, '')
-          .gsub(%r{^\/}, '')}"
+      def path_params(params)
+        "/#{params[:path]
+              .gsub(/^#{I18n.available_locales.join("|")}/, '')
+              .gsub(%r{^\/}, '')}"
+      end
+    end
   end
 end
--- lib/murloc/helpers.rb
+++ lib/murloc/helpers.rb
@@ -1,16 +1,18 @@
-# Mix it to add helpers to change locale
-module Murloc::Helpers
-  def self.included(base)
-    return unless respond_to?(:helper_method)
+module Murloc
+  # Mix it to add helpers to change locale
+  module Helpers
+    def self.included(base)
+      return unless respond_to?(:helper_method)
 
-    base.class_eval do
-      helper_method :link_to_change_locale
+      base.class_eval do
+        helper_method :link_to_change_locale
+      end
     end
-  end
 
-  def link_to_change_locale(name = nil, options = nil, html_options = nil, &block)
-    options_with_locale = "#{I18n.locale}/" + options
-    context = respond_to?(:link_to) ? self : view_context
-    context.link_to(name, options_with_locale, html_options, &block)
+    def link_to_change_locale(name = nil, options = nil, html_options = nil, &block)
+      options_with_locale = "#{I18n.locale}/" + options
+      context = respond_to?(:link_to) ? self : view_context
+      context.link_to(name, options_with_locale, html_options, &block)
+    end
   end
 end
