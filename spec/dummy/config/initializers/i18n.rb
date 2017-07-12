6c1a3eb30eea4eceacd56b6e178d365c478d1bc8
Dmitrii Topornin <dtopornin@gmail.com>
2017-07-12 22:07:22 +0300

    add some capybara specs

---
+++ spec/dummy/config/initializers/i18n.rb
@@ -0,0 +1,2 @@
+I18n.available_locales = %i[en ru]
+I18n.default_locale = :en
--- spec/murloc/features/change_locale_spec.rb
+++ spec/murloc/features/change_locale_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Change locale', type: :feature do
   scenario 'User visit page' do
     visit '/'
+    expect(page.find('#index').text).to eq('en ru')
   end
 end
