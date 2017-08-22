28fc108d9bd5885ce12df36f55e5fb07c9679fe0
Dmitrii Topornin <dtopornin@gmail.com>
2017-08-23 01:31:18 +0300

    New specs

--- spec/murloc/features/change_locale_spec.rb
+++ spec/murloc/features/change_locale_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Change locale', type: :feature do
   scenario 'User visit page' do
     visit '/'
-    expect(page.find('#index').text).to eq('en ru')
+    expect(page.find('#current_locale').text).to eq 'en'
+    click_link 'ru'
+    expect(page.find('#current_locale').text).to eq 'ru'
   end
 end
